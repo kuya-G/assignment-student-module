9e9b72a7ff7bca97748ae7e9fe648f7a3061a675
kuya-G <dayandante.jeric@gmail.com>
2025-01-12 01:19:01 +0800

    Item Deleted

--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -10,13 +10,11 @@ export class StudentService {
         private readonly studentRepository: Repository<Student>,
     ) {}
 
-    // create
     async createStudent(date: Partial<Student>): Promise<Student> {
         const student = this.studentRepository.create(date);
         return this.studentRepository.save(student);
     }
 
-    // read
     async getStudents(): Promise<Student[]> {
         return this.studentRepository.find();
     }
@@ -29,10 +27,16 @@ export class StudentService {
         return student;
       }
     
-    // update
     async updateStudent(id: number, data: Partial<Student>): Promise<Student> {
         const student = await this.getStudentById(id);
         Object.assign(student, data);
         return this.studentRepository.save(student);
+    }
+
+    async deleteStudent(id: number): Promise<void> {
+        const result = await this.studentRepository.delete(id);
+        if (result.affected === 0) {
+            throw new NotFoundException(`student with ID ${id} not found`)
+        }
     }
 }
